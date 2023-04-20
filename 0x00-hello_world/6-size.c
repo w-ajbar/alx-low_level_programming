a8dfded7bee20308e1f6118fe01ec87a682faef8
w-ajbar <wassila.ajbar@gmail.com>
2023-04-20 16:10:36 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - 6-size.c
+ * description - Write a C program that prints the size of various types on
+ * the computer it is compiled and run on.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
