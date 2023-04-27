6dc49875eecc2d93b4b3661e45770fbc1f1b7296
w-ajbar <wassila.ajbar@gmail.com>
2023-04-26 18:08:14 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 
-int _purchar(char);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -10,9 +10,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times:table(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-print_times_table(int n);
+void print_times_table(int n);
 
 #endif
